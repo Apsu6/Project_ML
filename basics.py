4c0633d4e08126ddaffc9e242af9b35397d2a495
Abbas Muhi <igarashieg122119@gmail.com>
2026-07-12 15:37:21 +0300

    Emptied my basics.py file for my new DSA basics course.

--- basics.py
+++ basics.py
@@ -1,7 +1,26 @@
-# Basic Python script
+"""This file will contain my trials to understanding basic DSA in python.
 
-def cock():
-    print("cock")
+A function that finds the minimum value."""
 
 
-cock()
+def find_minimum(nums):
+    """Return the minimum value in the given iterable of numbers.
+
+    Args:
+        nums (iterable): An iterable of comparable numeric values.
+
+    Returns:
+        The smallest value in nums, or None if nums is empty.
+    """
+    if len(nums) == 0:
+        return None
+
+    min_val = float("inf")
+    for num in nums:
+        if num < min_val:
+            min_val = num
+    return min_val
+
+
+lst = [3, -1, 4, 1, 5, 9, 2, 6, 50]
+print(find_minimum(lst))
